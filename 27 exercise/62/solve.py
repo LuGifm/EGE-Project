1b3dfcc470d0dc4cfd684046439cc3b9f056652a
LuGifm <vana0kor2@gmail.com>
2016-05-12 23:30:41 +0300

    Added 62 exercise from 27.

--- 27 exercise/62/solve.py
+++ 27 exercise/62/solve.py
@@ -1,10 +1,33 @@
 """
 Задачка не имеет отношение к егэ.
 В питоне есть встроенные функции решающие задачу оптимально (bin(), list.reverse())
-Задча проверяет знание основных конструкций низкоуровнего языка (С++, Си).
+Различия между программами на разных языках будут огромны.
+Задча проверяет знание основных конструкций низкоуровнего языка (С++, Си). std::cin.get() к примеру
+Кроме того, полезно знать что такое конечный автомат.
+
 """
 n = int(input())
-n = list(bin(n)[2:]).reverse()
+n = list(bin(n)[2:])
+n.reverse()
 n.pop()
+n = list(map(int,n))
 s = 0
+is_end = True
+count = 0
 for x in range(len(n)):
+    if is_end == True:
+        if n[x] == 0:
+            print('E', end='')
+            count += 1
+        elif n[x] == 1:
+            is_end = False
+    else:
+        if n[x] == 1:
+            is_end = True
+            print('A', end='')
+            count += 1
+        elif n[x] == 0:
+            is_end = True
+            print('P', end='')
+print('')
+print(count)
